3780d1f84623fee5d596aaa355f38506d0567c6c
edrandall-dev <github@edrandall.uk>
2020-07-10 14:35:07 +0100

    New border dimensions (1280x800)

--- pong.py
+++ pong.py
@@ -5,7 +5,7 @@ import pygame
 pygame.init()
 
 #Set some variables
-WIDTH=800
+WIDTH=1200
 HEIGHT=800
 BORDER=20
 
@@ -14,11 +14,14 @@ screen = pygame.display.set_mode((WIDTH,HEIGHT))
 bordercol = pygame.Color("white")
 
 #Draw the board
+
+##Define each rectangle which will be drawn to make up the border
 rect1 = pygame.Rect((0,0),(WIDTH,BORDER))
 rect2 = pygame.Rect((0,780),(WIDTH,BORDER))
 rect3 = pygame.Rect((0,0),(BORDER,HEIGHT))
-rect4 = pygame.Rect((780,0),(BORDER,HEIGHT))
+rect4 = pygame.Rect((1180,0),(BORDER,HEIGHT))
 
+#Actually draw each rectangle to make the border
 pygame.draw.rect(screen, bordercol, rect1)
 pygame.draw.rect(screen, bordercol, rect2)
 pygame.draw.rect(screen, bordercol, rect3)
