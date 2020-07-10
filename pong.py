549b41d47cf0460eb44bb9fe4546b74e65f30015
edrandall-dev <github@edrandall.uk>
2020-07-10 14:06:42 +0100

    Complete border drawn

--- pong.py
+++ pong.py
@@ -1,9 +1,27 @@
+#!/usr/bin/python2.7
+
 import pygame
 
 pygame.init()
 
-WIDTH=500
-HEIGHT=500
+#Set some variables
+WIDTH=800
+HEIGHT=800
+BORDER=20
 
+#Draw the playing area
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
+bordercol = pygame.Color("white")
 
+#Draw the board
+rect1 = pygame.Rect((0,0),(WIDTH,BORDER))
+rect2 = pygame.Rect((0,780),(WIDTH,BORDER))
+rect3 = pygame.Rect((0,0),(BORDER,HEIGHT))
+rect4 = pygame.Rect((780,0),(BORDER,HEIGHT))
+
+pygame.draw.rect(screen, bordercol, rect1)
+pygame.draw.rect(screen, bordercol, rect2)
+pygame.draw.rect(screen, bordercol, rect3)
+pygame.draw.rect(screen, bordercol, rect4)
+
+pygame.display.flip()
